78a2250ae71dcf48ea5d6e16829c317a8df3362a
Muljadi <muljadiyadi29@gmail.com>
2021-05-26 12:06:08 +0700

    First commit

---
+++ assetManagement/assetManagement/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assetManagement.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
