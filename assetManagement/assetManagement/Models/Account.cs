7428ac729ed484ab0ba579e9df82d3b84b085e87
Muhammad Jurnalies Habibie <habibie598@gmail.com>
2021-05-26 16:44:46 +0700

    Update Account

--- assetManagement/assetManagement/Models/Account.cs
+++ assetManagement/assetManagement/Models/Account.cs
@@ -1,12 +1,16 @@
 ﻿ using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace assetManagement.Models
 {
+    [Table("TB_M_Account")]
     public class Account
     {
+        [Key]
         public int Id { get; set; }
         public string Password { get; set; }
         public Employee Employee { get; set; }
